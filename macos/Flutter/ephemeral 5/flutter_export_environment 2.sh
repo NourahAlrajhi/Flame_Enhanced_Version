b270671106355798c143c7270e9a23caa3e7c0c3
Nourah <98350756+NourahAlrajhi@users.noreply.github.com>
2023-04-05 00:26:27 +0300

    Add files via upload

---
+++ macos/Flutter/ephemeral 5/flutter_export_environment 2.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/eng.noni/development/Flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/eng.noni/Desktop/47406_G1_Flame"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/eng.noni/Desktop/47406_G1_Flame/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/eng.noni/Desktop/47406_G1_Flame/.dart_tool/package_config.json"
